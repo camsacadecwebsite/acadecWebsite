c7ccfd5560bcede0e30c3d5cf96cec69c0972f45
ladyforrest591 <laurenjoykim@gmail.com>
2026-09-20 16:49:09 -0700

    more update

--- java/main.js
+++ java/main.js
@@ -845,7 +845,7 @@ document.addEventListener('DOMContentLoaded', () => {
     var overallScoreDisplay = document.getElementById('overallScoreDisplay')
     if (overallScoreDisplay) {
         // Acadec Analytics Transfer to Page
-        var analyticsSpreadsheet = 'https://script.google.com/macros/s/AKfycbyYfl-dvkzzVdv5Wlh2rpWbWR-LjTSPFxtEZEGuvnPMaXD_O94ZQGvuBVK8FI0NREyL/exec';
+        var analyticsSpreadsheet = 'https://script.google.com/macros/s/AKfycbyspwgaHT-Jw7BXShlzTkN6z_UURy1LpK5YzSORsJzVZTORPVHDrbg2ZIBBx45OrzVY/exec';
         var idNumber = localStorage.getItem('activeHubID');
 
         if (!idNumber) {
